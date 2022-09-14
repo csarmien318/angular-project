7a06b084fe85a015859bd3b04d4e06f864a3b3b5
Christopher Sarmiento <csarmiento@rccl.com@c02xk3spjg5h.na.rccl.com>
2022-09-14 13:37:00 -0400

    debug error task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers;
+  // ERROR: servers was declared as
+  // servers;
+
+  // SOLUTION:
+  servers = [];
+  count = 0;
 
   onAddServer() {
-    this.servers.push('Another Server');
+    this.servers.push(`Another Server ${++this.count}`);
   }
 
   onRemoveServer(id: number) {
     const position = id + 1;
-    this.servers.splice(position, 1);
+
+    // ERROR: 
+    // const deleted = this.servers.splice(position, 1);
+
+    // SOLUTION:
+    const deleted = this.servers.splice(position - 1, 1);
+    this.count = this.servers.length === 0 ? 0 : this.count;
   }
 }
